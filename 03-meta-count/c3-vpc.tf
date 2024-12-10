42242eb41c7da7d4b7d9a78a4b4b54a9e0f38cde
AkshayFasale <fasaleakshay25@gmail.com>
2024-12-10 19:09:49 +0530

    Meta Arg

---
+++ 03-meta-count/c3-vpc.tf
@@ -0,0 +1,27 @@
+# Resource block for VPC
+
+resource "google_compute_network" "myVPC" {
+  name                    = "vpc1"
+  auto_create_subnetworks = false # defines VPC is manual
+  project                 = "terraform-practice-436907"
+
+
+}
+
+# resource block for subnet
+
+resource "google_compute_subnetwork" "mysubnet1" {
+  name          = "subnet1"
+  region        = "us-central1"
+  provider      = google.dev
+  ip_cidr_range = "10.128.0.0/20"
+  network       = google_compute_network.myVPC.id
+}
+
+resource "google_compute_subnetwork" "mysubnet2" {
+  name          = "subnet2"
+  region        = "us-central1"
+  provider      = google.dev
+  ip_cidr_range = "10.132.0.0/20"
+  network       = google_compute_network.myVPC.id
+}
---
+++ 03-meta-count/c5-vm-instance.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "myapp1" {
+  project      = var.gcp-project
+  name         = "myapp-${count.index}"
+  machine_type = var.machine_type
+  count        = 2
+  zone         = "us-central1-a"
+  tags         = [tolist(google_compute_firewall.fw_ssh.target_tags)[0], tolist(google_compute_firewall.fw-http.target_tags)[0]]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.mysubnet1.id
+    access_config {
+
+    }
+  }
+
+
+}
