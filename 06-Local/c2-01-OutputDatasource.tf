9a9ec12a88a0c8b96f166832afe6797da02ffa6e
AkshayFasale <fasaleakshay25@gmail.com>
2024-12-12 12:09:19 +0530

    image Datasource

---
+++ 06-Local/c2-01-OutputDatasource.tf
@@ -0,0 +1,45 @@
+data "google_compute_image" "debian" {
+  family  = "debian-12"
+  project = "debian-cloud"
+}
+
+output "id" {
+  value = data.google_compute_image.debian.id
+}
+output "self_link" {
+  value = data.google_compute_image.debian.self_link
+}
+output "name" {
+  value = data.google_compute_image.debian.name
+}
+output "family" {
+  value = data.google_compute_image.debian.family
+}
+output "image_id" {
+  value = data.google_compute_image.debian.image_id
+}
+output "status" {
+  value = data.google_compute_image.debian.status
+}
+output "liscense" {
+  value = data.google_compute_image.debian.licenses
+}
+output "description" {
+  value = data.google_compute_image.debian.description
+}
+output "project" {
+  value = data.google_compute_image.debian.project
+}
+output "source_image_id" {
+  value = data.google_compute_image.debian.source_image_id
+}
+
+# using MAP Key Value pair we can make code small for same output.
+output "vm-image-info" {
+  value = {
+    project         = data.google_compute_image.debian.project
+    source_image_id = data.google_compute_image.debian.source_image_id
+
+  }
+
+}
